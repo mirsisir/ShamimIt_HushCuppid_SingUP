45e38dc657864583641567e15739859ef7d65129
mirsisir <mirsisir@gmail.com>
2021-12-22 18:46:38 +0600

    test remote sql

--- app/Http/Controllers/WebSiteController.php
+++ app/Http/Controllers/WebSiteController.php
@@ -9,6 +9,12 @@ class WebSiteController extends Controller
 {
     public function dashboard(){
 
+        $con = mysqli_connect('139.162.54.146' ,'hushcupid_main','m8a?Md1e{jha','hushcupid_main');
+
+        $sql ="SELECT * FROM ph7_admins";
+        $res = mysqli_query($con,$sql);
+
+        dd($res);
 
         return view('dashboard');
     }
